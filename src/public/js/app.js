06e24bb8338efafe1027cdd5d2bd3c5e81cc7f03
ksw <tlsdnddl12@naver.com>
2022-07-31 16:37:19 +0900

    [Add] used socket.io and added room name function

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,44 +1,70 @@
-const nickForm = document.querySelector("#nick");
-const messageList = document.querySelector("ul");
-const messageForm = document.querySelector("#message");
-const socket = new WebSocket(`ws://${window.location.host}`); // JS 내장 객체 WebSocket을 이용해 소켓을 생성. 인자로는 연결되는 웹의 url을 담는다.
+// const nickForm = document.querySelector("#nick");
+// const messageList = document.querySelector("ul");
+// const messageForm = document.querySelector("#message");
+// const socket = new WebSocket(`ws://${window.location.host}`); // JS 내장 객체 WebSocket을 이용해 소켓을 생성. 인자로는 연결되는 웹의 url을 담는다.
 
-function makeMessage(type, payload) {
-  const msg = { type, payload };
-  return JSON.stringify(msg);
+// function makeMessage(type, payload) {
+//   const msg = { type, payload };
+//   return JSON.stringify(msg);
+// }
+
+// socket.addEventListener("open", () => {
+//   console.log("Connected to Server");
+// });
+
+// socket.addEventListener("message", (message) => {
+//   const li = document.createElement("li");
+//   li.innerText = message.data;
+//   messageList.append(li);
+// });
+
+// socket.addEventListener("close", () => {
+//   console.log("Disconnected from the server");
+// });
+
+// // setTimeout(() => {
+// //   socket.send("hello from browser!");
+// // }, 5000);
+
+// function handleSubmit(event) {
+//   event.preventDefault();
+//   const input = messageForm.querySelector("input");
+//   socket.send(makeMessage("new_message", input.value));
+//   input.value = "";
+// }
+
+// function handleNickSubmit(event) {
+//   event.preventDefault();
+//   const input = nickForm.querySelector("input");
+//   socket.send(makeMessage("nickname", input.value));
+//   input.value = "";
+// }
+
+// messageForm.addEventListener("submit", handleSubmit);
+// nickForm.addEventListener("submit", handleNickSubmit);
+const socket = io(); // 자동으로 socket.io를 실행하고 있는 서버를 찾는다.
+
+const welcome = document.getElementById("welcome");
+const form = welcome.querySelector("form");
+const room = document.getElementById("room");
+
+room.hidden = true;
+
+let roomName;
+
+function showRoom() {
+  welcome.hidden = true;
+  room.hidden = false;
+  const h3 = room.querySelector("h3");
+  h3.innerText = `Room ${roomName}`;
 }
 
-socket.addEventListener("open", () => {
-  console.log("Connected to Server");
-});
-
-socket.addEventListener("message", (message) => {
-  const li = document.createElement("li");
-  li.innerText = message.data;
-  messageList.append(li);
-});
-
-socket.addEventListener("close", () => {
-  console.log("Disconnected from the server");
-});
-
-// setTimeout(() => {
-//   socket.send("hello from browser!");
-// }, 5000);
-
-function handleSubmit(event) {
+function handleRoomSubmit(event) {
   event.preventDefault();
-  const input = messageForm.querySelector("input");
-  socket.send(makeMessage("new_message", input.value));
+  const input = form.querySelector("input");
+  socket.emit("enter_room", input.value, showRoom);
+  roomName = input.value;
   input.value = "";
 }
 
-function handleNickSubmit(event) {
-  event.preventDefault();
-  const input = nickForm.querySelector("input");
-  socket.send(makeMessage("nickname", input.value));
-  input.value = "";
-}
-
-messageForm.addEventListener("submit", handleSubmit);
-nickForm.addEventListener("submit", handleNickSubmit);
+form.addEventListener("submit", handleRoomSubmit);
--- src/server.js
+++ src/server.js
@@ -1,6 +1,8 @@
 import express from "express";
 import http from "http";
-import WebSocket from "ws";
+import SocketIO from "socket.io";
+
+// import WebSocket from "ws";
 
 const app = express();
 
@@ -9,29 +11,46 @@ app.set("views", __dirname + "/views");
 app.use("/public", express.static(__dirname + "/public"));
 app.get("/", (req, res) => res.render("home"));
 app.get("/*", (req, res) => res.redirect("/"));
-const handleListen = () => console.log("Listening on port 3000");
+// const handleListen = () => console.log("Listening on port 3000");
 //app.listen(3000, handleListen);
-const server = http.createServer(app);
-const wss = new WebSocket.Server({ server });
-const sockets = [];
-wss.on("connection", (socket) => {
-  socket["nickname"] = "Anonymous";
-  sockets.push(socket);
-  console.log("connected to browser");
-  socket.on("close", () => console.log("Disconnected from the Browser"));
-  socket.on("message", (msg) => {
-    const message = JSON.parse(msg);
-    switch (message.type) {
-      case "new_message":
-        sockets.forEach((aSocket) =>
-          aSocket.send(`${socket.nickname}: ${message.payload}`),
-        );
-        break;
-      case "nickname":
-        socket["nickname"] = message.payload;
-        break;
-    }
-  });
-}); // connection 이벤트가 발생하면 handleConnection 함수를 실행. on 메서드는 콜백 함수에 socket을 넘겨준다. 소켓은 사용자와 서버의 연결 혹은 연결에 대한 정보를 의미.
+// const server = http.createServer(app);
+const httpServer = http.createServer(app);
+const wsServer = SocketIO(httpServer);
 
-server.listen(3000, handleListen); // http와 웹소켓 프로토콜을 하나의 포트에서 사용하려면 http 서버 위에 웹소켓 서버를 만들어 준다.
+// const wss = new WebSocket.Server({ server });
+// const sockets = [];
+// wss.on("connection", (socket) => {
+//   socket["nickname"] = "Anonymous";
+//   sockets.push(socket);
+//   console.log("connected to browser");
+//   socket.on("close", () => console.log("Disconnected from the Browser"));
+//   socket.on("message", (msg) => {
+//     const message = JSON.parse(msg);
+//     switch (message.type) {
+//       case "new_message":
+//         sockets.forEach((aSocket) =>
+//           aSocket.send(`${socket.nickname}: ${message.payload}`),
+//         );
+//         break;
+//       case "nickname":
+//         socket["nickname"] = message.payload;
+//         break;
+//     }
+//   });
+// }); // connection 이벤트가 발생하면 handleConnection 함수를 실행. on 메서드는 콜백 함수에 socket을 넘겨준다. 소켓은 사용자와 서버의 연결 혹은 연결에 대한 정보를 의미.
+wsServer.on("connection", (socket) => {
+  socket.on("enter_room", (roomName, done) => {
+    done();
+    console.log(roomName);
+    console.log(socket.id);
+    console.log(socket.rooms);
+    socket.join(roomName);
+    console.log(socket.rooms);
+    // setTimeout(() => {
+    //   done();
+    // }, 5000);
+  });
+});
+
+const handleListen = () => console.log("Listening on 3000");
+httpServer.listen(3000, handleListen); // http와 웹소켓 프로토콜을 하나의 포트에서 사용하려면 http 서버 위에 웹소켓 서버를 만들어 준다.
